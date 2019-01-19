fe7f63414395d1b9e2f19cf1e72ce2abcdbe39a8
Azis18 <44446834+azis1234@users.noreply.github.com>
2019-01-19 13:16:15 +0700

    config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+$sign = "084b3ef0e172b7b0b4cbcebf818fd00c6bc6b2bc";
+$aid = "d8bf7cd7-e4b0-43c6-91ad-bc27497b0e75";
+$uuid = "706d1dbbae61ff6ef526ad4a2f3a5864bb77ed64";
+$android_id = "2a9e436b0fa5fc6";
+?>
